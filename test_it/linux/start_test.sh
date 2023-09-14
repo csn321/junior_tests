b0b98827b17dc42a3bbf28e8733431510d50cd4c
Cheboksarov Sergey <csn@lab321.ru>
2023-09-14 16:21:29 +0600

    bug fix

--- test_it/linux/start_test.sh
+++ test_it/linux/start_test.sh
@@ -1,19 +1,44 @@
 #!/bin/bash
 
 export NLS_LANG=AMERICAN_AMERICA.UTF8
-export PATH=/home/csn/instantclient_21_3:$PATH
-export LD_LIBRARY_PATH=/home/csn/instantclient_21_3:$LD_LIBRARY_PATH
-export TNS_ADMIN=/home/csn
+
+echo -n "Do you have properly installed oracle client(Y|N) (sqlplus start with no problem)? [Y]: "
+read ORA_CLIENT_
+
+if [ "$ORA_CLIENT_" == "N" ]; then
+
+   ORACLE_CLIENT_PATH="/home/csn/instantclient_21_3"
+   TNS_ADMIN="/home/csn"
+
+   echo -n "Enter path to oracle client (default $ORACLE_CLIENT_PATH) and press enter: "
+   read ORACLE_CLIENT_PATH_
+
+   if [ -n "$ORACLE_CLIENT_PATH_" ]; then
+      ORACLE_CLIENT_PATH=$ORACLE_CLIENT_PATH_
+   fi
+
+   echo -n "Enter path to tnsnames.ora (default $TNS_ADMIN) and press enter: "
+   read TNS_ADMIN_
+
+   if [ -n "$TNS_ADMIN_" ]; then
+      TNS_ADMIN=$TNS_ADMIN_
+   fi
+
+   export PATH=$ORACLE_CLIENT_PATH:$PATH
+   export LD_LIBRARY_PATH=$ORACLE_CLIENT_PATH:$LD_LIBRARY_PATH
+   export TNS_ADMIN=$TNS_ADMIN
+
+fi
 
 echo -n "Enter test number (1, 2, 3) and press enter: "
 read test_number
 
-echo "Execute test" $test_number
-
-if [ $test_number -lt 1 ] || [ $test_number -gt 3 ]; then
+if [ -z "$test_number" ] || ! [[ $test_number =~ "[1-3]{1}" ]]; then
    echo "Error. Incorrect test number!"
    exit 1
 fi
+
+echo "Execute test" $test_number
 
 echo -n "Enter the connect string (username@tnsname) and press enter: "
 read orauid
